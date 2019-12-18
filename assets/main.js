e5f1f3c7391b53cdae0080a79345caa41a539692
aidensymes <hello@aidensymes.com>
2019-12-18 12:04:22 -0500

    more steup

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('a').click(function(e) {
+    if ($(this).attr('target') != "_blank") {
+      e.preventDefault();
+      $('.overlay').css('left', 'initial');
+      $('.overlay').css('right', '0');
+      $('.overlay').css('width', '100%');
+      var href = $(this).attr('href');
+      setTimeout(function() {
+        window.location = href;
+      }, 150);
+      return false;
+    }
+  });
+});
