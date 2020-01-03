6674aa07719605aaac9caaaa0d3d78488080a2ad
aidensymes <hello@aidensymes.com>
2020-01-03 12:18:50 -0500

    mobile refinements

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,19 @@
 $(window).on('load', function() {
-  jQuery('img.svg').each(function(){
-    var $img = jQuery(this);
+  $('img.svg').each(function(){
+    var $img = $(this);
     var imgID = $img.attr('id');
     var imgClass = $img.attr('class');
     var imgURL = $img.attr('src');
-    jQuery.get(imgURL, function(data) {
-        var $svg = jQuery(data).find('svg');
-        if (typeof imgID !== 'undefined') {
-          $svg = $svg.attr('id', imgID);
-        }
-        if (typeof imgClass !== 'undefined') {
-          $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-        $svg = $svg.removeAttr('xmlns:a');
-        $img.replaceWith($svg);
+    $.get(imgURL, function(data) {
+      var $svg = $(data).find('svg');
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+      $svg = $svg.removeAttr('xmlns:a');
+      $img.replaceWith($svg);
     }, 'xml');
   });
 });
